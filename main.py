8095f1f78d04da7af0252525e960fc4a67270af4
Hendrik Pfaff <email@hendrik-pfaff.de>
2020-06-02 12:40:15 +0200

    TF tutorial for image classification.

--- main.py
+++ main.py
@@ -1,9 +1,67 @@
 # Copyright (C) 2020 Hendrik Pfaff
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+# Source reference: https://www.tensorflow.org/tutorials/images/cnn
+
+# Necessary packages.
 import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
 
-tf.add(1, 2).numpy()
+# Download the dataset (60k colored images ~163 MB). This may take a while.
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
-hello = tf.constant('Hello, TensorFlow!')
-hello.numpy()
+# Normalize pixel values to be between 0 and 1.
+#train_images, test_images = train_images / 255.0, test_images / 255.0
+
+# Define the names of the 10 classes.
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+# Print the first 25 images.
+plt.figure(figsize=(25, 25))
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    # The CIFAR labels happen to be arrays, which is why you need the extra index.
+    plt.xlabel(class_names[train_labels[i][0]])
+plt.show()
+
+# Creating (convolutional) Layers of the model.
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+# Show the model at this stage.
+model.summary()
+
+# Add Dense Layers.
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+
+model.summary()
+
+# Compile.
+model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+
+# Train model.
+history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+
+# Evaluate model performance.
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print(test_acc)
+print(test_loss)
